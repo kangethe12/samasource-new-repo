cd1bd4b28b1cd51f3b427bac994fc0bba627bc0c
kangethe12 <kangethemujohn@gmail.com>
2017-10-13 16:28:53 +0300

    added comments in divtest

--- div_test.py
+++ div_test.py
@@ -1,18 +1,22 @@
 import unittest
 
 from div import divid
+"""testing my div.py"""
 class testdivision(unittest.TestCase):
-  def test_dividing(self):
-    self.assertEqual(divid(10,2),5)
-    self.assertEqual(divid(-6,3),-2)
-  def test_divide_by_zero(self):
-    try:
-      divid(12,0)
-    except ZeroDivisionError:
-      self.fail("can't divide by zero")
-  def test_edgecase(self):
-    self.assertEqual(divid(1000,-2),-500)
-    self.assertEqual(divid(-500,-250),2)
+    """testing normal and small numbers"""
+    def test_dividing(self):
+        self.assertEqual(divid(10,2),5)
+        self.assertEqual(divid(-6,3),-2)
+    """testing the division by zero"""
+    def test_divide_by_zero(self):
+        try:
+          divid(12,0)
+        except ZeroDivisionError:
+            self.fail("can't divide by zero")
+    """testing the division by extreme numbers"""
+    def test_edgecase(self):
+        self.assertEqual(divid(1000,-2),-500)
+        self.assertEqual(divid(-500,-250),2)
 
 
 if __name__ == '__main__':
